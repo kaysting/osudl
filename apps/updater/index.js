50249ae89f6255dc2d035998db14239dc4f6ef6f
Kayla Kersting <kayla@kaysting.dev>
2026-06-08 00:32:40 -0500

    Don't use the osu API if it's inaccessible

--- apps/updater/index.js
+++ apps/updater/index.js
@@ -2,18 +2,43 @@ const db = require('#db');
 const utils = require('#utils');
 const ingest = require('#api/ingest.js');
 
+const axios = require('axios');
+let isOsuOnline = null;
+const checkOsuAccessibility = async () => {
+    const oldStatus = isOsuOnline;
+    let statusCode = null;
+    try {
+        await axios.get('https://osu.ppy.sh/api/v2/auth/token');
+        isOsuOnline = true;
+    } catch (error) {
+        statusCode = error.response?.status;
+        isOsuOnline = statusCode === 401;
+    }
+    if (oldStatus !== isOsuOnline) {
+        if (isOsuOnline) {
+            utils.log(`osu! API is online`);
+        } else {
+            utils.logError(
+                `osu! API is currently inaccessible (error ${statusCode}), updates will be delayed until access is restored`
+            );
+        }
+    }
+    setTimeout(checkOsuAccessibility, 1000 * 60);
+    return isOsuOnline;
+};
+
 // Import maps from dump if it's been over a month since the last import
 const runImport = async () => {
     const lastImportTime = parseInt(utils.readMiscData('last_dump_import_time') || '0');
     const oneMonth = 1000 * 60 * 60 * 24 * 30;
-    if (Date.now() - lastImportTime > oneMonth) {
+    if (isOsuOnline && Date.now() - lastImportTime > oneMonth) {
         await ingest.importFromDump();
     }
     setTimeout(runImport, 1000 * 60 * 60 * 24);
 };
 
 const runRecents = async () => {
-    if (utils.readMiscData('last_dump_import_time')) {
+    if (isOsuOnline && utils.readMiscData('last_dump_import_time')) {
         await ingest.importFromRecents();
     }
     setTimeout(runRecents, 1000 * 60);
@@ -21,7 +46,7 @@ const runRecents = async () => {
 
 // Check all saved maps against the osu API for changes
 const runFullScan = async () => {
-    if (utils.readMiscData('last_dump_import_time')) {
+    if (isOsuOnline && utils.readMiscData('last_dump_import_time')) {
         await ingest.scanForChanges();
         setTimeout(runFullScan, 1000 * 60 * 60 * 24);
     } else {
@@ -32,7 +57,7 @@ const runFullScan = async () => {
 // Frequently scan recently ranked maps for changes
 // This allows us to reflect unranks/updates quickly
 const runRecentScan = async () => {
-    if (utils.readMiscData('last_dump_import_time')) {
+    if (isOsuOnline && utils.readMiscData('last_dump_import_time')) {
         const oneWeekAgo = Date.now() - 1000 * 60 * 60 * 24 * 7;
         await ingest.scanForChanges(oneWeekAgo);
     }
@@ -40,6 +65,9 @@ const runRecentScan = async () => {
 };
 
 (async () => {
+    // Check osu API status
+    await checkOsuAccessibility();
+
     // Start these immediately
     runImport();
     runRecents();
